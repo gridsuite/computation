a63d42e871b8e84245379770a9a39cbe53f7a36b
Etienne LESOT <etienne.lesot@rte-france.com>
2026-09-23 08:53:20 +0200

    add preloading status
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/main/java/org/gridsuite/computation/service/NotificationService.java
+++ src/main/java/org/gridsuite/computation/service/NotificationService.java
@@ -32,6 +32,7 @@ public class NotificationService {
     private static final String RUN_CATEGORY_BROKER_OUTPUT = NotificationService.class.getName() + ".output-broker-messages.run";
     private static final String STOP_CATEGORY_BROKER_OUTPUT = NotificationService.class.getName() + ".output-broker-messages.stop";
     private static final String RESULT_CATEGORY_BROKER_OUTPUT = NotificationService.class.getName() + ".output-broker-messages.result";
+    private static final String PRELOADING_CATEGORY_BROKER_OUTPUT = NotificationService.class.getName() + ".output-broker-messages.preloading";
     private static final String FAILED_CATEGORY_BROKER_OUTPUT = NotificationService.class.getName() + ".output-broker-messages.failed";
     private static final String CANCEL_FAILED_CATEGORY_BROKER_OUTPUT = NotificationService.class.getName() + ".output-broker-messages.cancel-failed";
 
@@ -39,6 +40,7 @@ public class NotificationService {
     private static final Logger CANCEL_MESSAGE_LOGGER = LoggerFactory.getLogger(CANCEL_CATEGORY_BROKER_OUTPUT);
     private static final Logger STOP_MESSAGE_LOGGER = LoggerFactory.getLogger(STOP_CATEGORY_BROKER_OUTPUT);
     private static final Logger RESULT_MESSAGE_LOGGER = LoggerFactory.getLogger(RESULT_CATEGORY_BROKER_OUTPUT);
+    private static final Logger PRELOADING_MESSAGE_LOGGER = LoggerFactory.getLogger(PRELOADING_CATEGORY_BROKER_OUTPUT);
     private static final Logger FAILED_MESSAGE_LOGGER = LoggerFactory.getLogger(FAILED_CATEGORY_BROKER_OUTPUT);
     private static final Logger CANCEL_FAILED_MESSAGE_LOGGER = LoggerFactory.getLogger(CANCEL_FAILED_CATEGORY_BROKER_OUTPUT);
 
@@ -95,6 +97,19 @@ public class NotificationService {
         Message<String> message = builder.build();
         RESULT_MESSAGE_LOGGER.debug(SENDING_MESSAGE, message);
         publisher.send(publishPrefix + "Result-out-0", message);
+    }
+
+    @PostCompletion
+    public void sendPreloadingFinishedMessage(UUID resultUuid, String receiver, String userId, @Nullable Map<String, Object> additionalHeaders) {
+        MessageBuilder<String> builder = MessageBuilder
+                .withPayload("")
+                .setHeader(HEADER_RESULT_UUID, resultUuid.toString())
+                .setHeader(HEADER_RECEIVER, receiver)
+                .setHeader(HEADER_USER_ID, userId)
+                .copyHeaders(additionalHeaders);
+        Message<String> message = builder.build();
+        PRELOADING_MESSAGE_LOGGER.debug(SENDING_MESSAGE, message);
+        publisher.send(publishPrefix + "Preloading-out-0", message);
     }
 
     @PostCompletion
